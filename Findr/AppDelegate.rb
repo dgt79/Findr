916710cd478f785152b1d31a509dbb3df164160a
Dan <dan.talpau@gmail.com>
2011-12-22 22:20:38 +0000

    Initial Commit

---
+++ Findr/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  Findr
+#
+#  Created by Dan on 22/12/2011.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
